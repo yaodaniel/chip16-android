73fd39d019e74cfda4bc6aa587a24d8dd103415a
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2013-10-31 13:52:56 -0700

    Added the some interfacing, needs more implementation.

---
+++ src/com/example/chip16/Cpu.java
@@ -0,0 +1,21 @@
+package com.example.chip16;
+
+public class Cpu {
+	//Registers
+	private short pc;						// Program counter
+	private short sp;						// Stack pointer
+	private short[] reg = new short[16];	// Data registers
+	private byte flags;						// Flag registers
+	
+	public void initCPU() {
+		//TODO
+	}
+	
+	public void fetchOpcode() {
+	//TODO
+	}
+	
+	public void runInstruction() {
+		//TODO
+	}
+}
---
+++ src/com/example/chip16/Memory.java
@@ -0,0 +1,5 @@
+package com.example.chip16;
+
+public class Memory {
+	
+}
